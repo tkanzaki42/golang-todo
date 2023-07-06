fc38e65cb4e101e55ca6627f2bb34cdb9a97d4ff
tkanzaki42 <tkanzaki@student.42tokyo.jp>
2023-07-06 13:25:48 +0900

    feat: CRUD OK

--- handler.go
+++ handler.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 
@@ -10,9 +10,13 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Todo struct {
-	ID    int64  `json:"id" validate:"required"`
-	Title string `json:"title" validate:"required"`
+type TodoResult struct {
+	ID   string `json:"id"`
+	Task string `json:"task"`
+}
+
+type TodoResultWithoutID struct {
+	Task string `json:"task"`
 }
 
 var client = redis.NewClient(&redis.Options{
@@ -24,28 +28,57 @@ var client = redis.NewClient(&redis.Options{
 func GetTodo(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-	idInt, err := strconv.ParseInt(id, 10, 64)
+
+	todoString, err := client.Get("todo:" + id).Result()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	todo, err := client.LIndex("todos", idInt).Result()
+	var todo TodoResultWithoutID
+	err = json.Unmarshal([]byte(todoString), &todo)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprint(w, todo)
+
+	getTodoResult := TodoResultWithoutID{
+		Task: todo.Task,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(getTodoResult)
 }
 
 func CreateTodo(w http.ResponseWriter, r *http.Request) {
-	param, err := ioutil.ReadAll(r.Body)
+	var todo TodoResult
+	err := json.NewDecoder(r.Body).Decode(&todo)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	err = client.RPush("todos", string(param)).Err()
+	nextID, err := client.Incr("todo:next_id").Result()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	todo.ID = strconv.FormatInt(nextID, 10)
+
+	todoJson, err := json.Marshal(todo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = client.Set("todo:"+todo.ID, string(todoJson), 0).Err()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = client.RPush("todos", todo.ID).Err()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -57,52 +90,77 @@ func CreateTodo(w http.ResponseWriter, r *http.Request) {
 func UpdateTodo(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-	idInt, err := strconv.ParseInt(id, 10, 64)
+
+	var newTodo TodoResult
+	err := json.NewDecoder(r.Body).Decode(&newTodo)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	param, err := ioutil.ReadAll(r.Body)
+	newTodo.ID = id
+
+	newTodoJson, err := json.Marshal(newTodo)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	err = client.LSet("todos", idInt, string(param)).Err()
+	err = client.Set("todo:"+id, string(newTodoJson), 0).Err()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprint(w, "Todo updated")
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func DeleteTodo(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	err := client.Del("todos", id).Err()
+	err := client.Del("todo:" + id).Err()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	fmt.Fprint(w, "Todo deleted")
+
+	err = client.LRem("todos", 0, id).Err()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func GetTodos(w http.ResponseWriter, r *http.Request) {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "mypassword",
-		DB:       0,
-	})
-
-	todos, err := client.LRange("todos", 0, -1).Result()
+	todoIds, err := client.LRange("todos", 0, -1).Result()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	fmt.Fprint(w, todos)
+	todos := []TodoResult{}
+	for _, id := range todoIds {
+		todo, err := client.Get("todo:" + id).Result()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var t TodoResult
+		err = json.Unmarshal([]byte(todo), &t)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		todos = append(todos, t)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todos)
 }
 
 func DeleteTodos(w http.ResponseWriter, r *http.Request) {
@@ -111,7 +169,7 @@ func DeleteTodos(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	fmt.Fprint(w, "Todos deleted")
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func HTTPHandler(w http.ResponseWriter, r *http.Request) {
