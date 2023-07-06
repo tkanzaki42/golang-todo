b5a17a4e66c2a7e6dbcd3f516c84f76095f57eee
tkanzaki42 <tkanzaki@student.42tokyo.jp>
2023-07-06 16:21:59 +0900

    test: 他CRUDテストOK

--- server_test.go
+++ server_test.go
@@ -87,5 +87,114 @@ func TestServer(t *testing.T) {
 		t.Fatalf("failed test. expected: %s, actual: %s", expected, todoResult.Task)
 	}
 
+	// PUT /api/todos
+	todo = map[string]string{
+		"task": "test2",
+	}
+	todoJson, _ = json.Marshal(todo)
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("PUT", "/api/todos/1", bytes.NewBuffer(todoJson))
+	r = mux.SetURLVars(r, map[string]string{
+		"id": "1",
+	})
+	UpdateTodo(w, r)
+	res = w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusNoContent {
+		t.Fatal("failed test: ", res.StatusCode)
+	}
+
+	// DELETE /api/todos/1
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("DELETE", "/api/todos/1", nil)
+	r = mux.SetURLVars(r, map[string]string{
+		"id": "1",
+	})
+	DeleteTodo(w, r)
+	res = w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusNoContent {
+		t.Fatal("failed test: ", res.StatusCode)
+	}
+
 	// GET /api/todos
+	// 2件登録
+	todo = map[string]string{
+		"task": "test",
+	}
+	todoJson, _ = json.Marshal(todo)
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("POST", "/api/todos", bytes.NewBuffer(todoJson))
+	CreateTodo(w, r)
+	res = w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusCreated {
+		t.Fatal("failed test: ", res.StatusCode)
+	}
+
+	todo = map[string]string{
+		"task": "test2",
+	}
+	todoJson, _ = json.Marshal(todo)
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("POST", "/api/todos", bytes.NewBuffer(todoJson))
+	CreateTodo(w, r)
+	res = w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusCreated {
+		t.Fatal("failed test: ", res.StatusCode)
+	}
+
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("GET", "/api/todos", nil)
+	GetTodos(w, r)
+	res = w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		t.Fatal("failed test: ", res.StatusCode)
+	}
+	b, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal("failed test")
+	}
+
+	var todos []TodoResultWithoutID
+	err = json.Unmarshal(b, &todos)
+	if err != nil {
+		t.Fatalf("failed to unmarshal response: %s", err)
+	}
+
+	if len(todos) != 2 {
+		t.Fatalf("failed test. expected: %d, actual: %d", 2, len(todos))
+	}
+
+	// DELETE /api/todos
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("DELETE", "/api/todos", nil)
+	DeleteTodos(w, r)
+	res = w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusNoContent {
+		t.Fatal("failed test: ", res.StatusCode)
+	}
+
+	// GET /api/todos
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("GET", "/api/todos", nil)
+	GetTodos(w, r)
+	res = w.Result()
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		t.Fatal("failed test: ", res.StatusCode)
+	}
+	b, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal("failed test")
+	}
+
+	err = json.Unmarshal(b, &todos)
+	if err != nil {
+		t.Fatalf("failed to unmarshal response: %s", err)
+	}
 }
